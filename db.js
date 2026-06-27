8ee789cf9cec650abee30568f1141735d3bebfad
Nadiyaghefira109 <withnadiya@gmail.com>
2026-06-27 13:11:27 +0700

    Fix database connection with SSL

--- db.js
+++ db.js
@@ -6,19 +6,9 @@ dotenv.config();
 const { Pool } = pg;
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_DATABASE,
-});
-
-// Tes koneksi ke database
-pool.connect((err) => {
-  if (err) {
-    console.error('❌ Gagal terhubung ke PostgreSQL:', err.message);
-  } else {
-    console.log('⚡ Sukses terhubung ke database PostgreSQL');
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false // Wajib untuk Neon.tech
   }
 });
 
